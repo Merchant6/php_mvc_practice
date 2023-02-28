2f35e01930119d03f1edef723acac6e282c94221
Merchant6 <saboormerchant6@gmail.com>
2023-03-01 03:05:18 +0500

    base Controller class added, modified the SiteController, and also modified the Router class

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,18 +2,36 @@
 
 namespace app\controllers;
 
+use app\core\Application;
+use app\core\Controller;
+
 /**
  * Class SiteController
  * 
  * @package app\controllers;
  */
 
-class Response
+class SiteController extends Controller
 {
-
-    public function setStatusCode(int $code)
+    public function home()
     {
-        http_response_code($code);
+        $params = 
+        [
+            'name' => "Saboor Merchant"
+        ];
+        
+        return $this->render('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
+    public function handleContact()
+    {
+        var_dump($_POST);
+        return 'Handling Submitted Data from Controller';
     }
     
 }
--- core/Request.php
+++ core/Request.php
@@ -27,4 +27,9 @@ class Request
     {
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
+
+    public function getBody()
+    {
+        
+    }
 }
