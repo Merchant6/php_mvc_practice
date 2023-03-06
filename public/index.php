94012af984f86980451adf4a1d0f041f8f7da9b7
Merchant6 <saboormerchant6@gmail.com>
2023-03-07 02:10:37 +0500

    Implemented the second layout and request body sanitization

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
 use app\controllers\SiteController;
+use app\controllers\AuthController;
 
 
 ini_set('display_errors', 1);
@@ -14,6 +15,14 @@ $app = new Application(dirname(__DIR__));
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
+
+
 
 
 $app->run();
